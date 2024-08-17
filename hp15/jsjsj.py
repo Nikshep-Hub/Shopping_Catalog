3fe35544dc58fcbe85e0e4c3f810238ceafcbc7d
Nikshep Bikkumalla <81143689+Nikshep-Hub@users.noreply.github.com>
2024-08-17 17:34:43 -0500

    Bolo1 (#11)
    * Newbranch (#2)
    
    * add file
    
    * add new
    
    * new command (#3)
    
    * Test1
    
    * Test1 (#5)
    
    sdv
    * dummy tes[2]
    
    * Test1 (#6)
    
    * dummy test[1]
    
    * dummy tes[2]
    
    * ascasc
    
    * sdvs
    
    * Test1 (#8)
    
    * first commit
    
    * second commit

--- hp15/jsjsj.py
+++ hp15/jsjsj.py
@@ -4,3 +4,11 @@ def greet(name):
 if __name__ == "__main__":
     name = input("Enter your name: ")
     print(greet(name))
+
+
+def greet1(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(greet1(name))
