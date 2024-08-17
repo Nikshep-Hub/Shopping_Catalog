ec759a2cb463832c42e37adc69162ead6dcd8e39
Nikshep Bikkumalla <81143689+Nikshep-Hub@users.noreply.github.com>
2024-08-17 17:35:34 -0500

    sdvsdc

---
+++ hp15/jsjsj.py
@@ -0,0 +1,14 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(greet(name))
+
+
+def greet1(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(greet1(name))
